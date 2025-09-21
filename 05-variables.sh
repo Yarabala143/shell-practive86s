77efc1f202a2824f486cd21c14f3d19ecec54a0b
unknown <Satish.yy@gmail.com>
2025-09-21 17:04:36 +0530

    v4

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Please enter your PIN number"
-read PIN_NUMBER # here PIN_NUMBER is variable
+read -s PIN_NUMBER # here PIN_NUMBER is variable
 echo "Your PIN number is $PIN_NUMBER"
