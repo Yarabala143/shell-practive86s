81b06cefe9c2866b22afd144cd49b0d5cf224e5b
unknown <Satish.yy@gmail.com>
2025-09-21 16:06:14 +0530

    v1

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: I am doing well, thanks for asking!"
+echo "Suresh:: That's great to hear!"
