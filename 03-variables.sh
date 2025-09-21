452ac3155a204d57ced62ef0ba406d80f8c6f093
unknown <Satish.yy@gmail.com>
2025-09-21 16:14:36 +0530

    v2

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=John
+PERSON2=Raheem
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am doing well, thanks for asking!"
+echo "$PERSON2:: That's great to hear!"
