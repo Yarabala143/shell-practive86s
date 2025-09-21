d491a67934557e3b57d953bf83cfd81de9a163af
unknown <Satish.yy@gmail.com>
2025-09-21 16:15:52 +0530

    v3

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=John
-PERSON2=Raheem
+PERSON1=Sachin
+PERSON2=Dhooni
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am doing well, thanks for asking!"
