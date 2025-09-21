da38ac134455ac7c58dc52af0740a45348f2f377
unknown <Satish.yy@gmail.com>
2025-09-21 16:58:24 +0530

    v4

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Arguments
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am doing well, thanks for asking!"
+echo "$PERSON2:: That's great to hear!"
