3c5f3c7e379ac97bc2dcafe6c9d450dfb0ac0824
unknown <Satish.yy@gmail.com>
2025-09-25 12:00:00 +0530

    variables

--- 12-installation.sh
+++ 12-installation.sh
@@ -3,12 +3,12 @@
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
     echo "Please run this script as root or using sudo"
-    #exit 1
+    exit 1
 fi
 dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "MySQL installation failed"
-    #exit 1
+    exit 1
 else
     echo "MySQL installed successfully"
 fi
